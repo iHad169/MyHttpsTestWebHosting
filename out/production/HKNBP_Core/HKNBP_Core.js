0fcd1526d03b91a834f7588c1f1d8015e9ebe9f0
iHad169 <27337315+iHad169@users.noreply.github.com>
2020-03-16 11:53:16 +0800

    test

--- out/production/HKNBP_Core/HKNBP_Core.js
+++ out/production/HKNBP_Core/HKNBP_Core.js
@@ -3186,7 +3186,7 @@ if (typeof kotlin === 'undefined') {
   }
   var rootURL;
   function coreVersion$lambda() {
-    return 'v2020.03_8-test2';
+    return 'v2020.03_8-test3';
   }
   var coreVersion;
   var appVersion;
