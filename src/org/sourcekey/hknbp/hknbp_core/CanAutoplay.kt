8616c13878218ecfdf8efd803f4cf154ef93b645
iHad169 <27337315+iHad169@users.noreply.github.com>
2020-05-04 16:29:12 +0800

    test

--- src/org/sourcekey/hknbp/hknbp_core/CanAutoplay.kt
+++ src/org/sourcekey/hknbp/hknbp_core/CanAutoplay.kt
@@ -30,13 +30,9 @@ object CanAutoplay {
     private fun isNotUseCanAutoplayOnThisRunner(): Boolean{
         var isNotUse = false
         //發現Tizen一運行CanAutoplay就會令頻道播放暫停,所以要篩走用Tizen系統嘅設備
-        if(RunnerInfo.getOsFamily() == "Tizen"){isNotUse = true;println("RunnerInfo.getOsFamily() == \"Tizen\" ${RunnerInfo.getOsFamily() == "Tizen"}")}
+        if(RunnerInfo.getOsFamily() == "Tizen"){isNotUse = true}
         //暫時未知webOS係米同Tizen發生同樣問題,孤奢試下唔行 (暫時未諗到點知行緊嘅裝置係webOS,試過getOsFamily()出"Linux x86")
-        if(0 < appVersion.match("/webOS/g")?.size?:0){isNotUse = true;println("0 < appVersion.match(\"/webOS/g\")?.size?:0 ${0 < appVersion.match("/webOS/g")?.size?:0}")}
-        println("appVersion ${appVersion}")
-        println("appVersion.match(\"/webOS/g\") ${appVersion.match("/webOS/g")}")
-        println("appVersion.match(\"/webOS/g\")?.size?:0 ${appVersion.match("/webOS/g")?.size?:0}")
-        println("isNotUse ${isNotUse}")
+        if(0 < appVersion.match("webOS")?.size?:0){isNotUse = true;println("ttttttt")}
         return isNotUse
     }
 
