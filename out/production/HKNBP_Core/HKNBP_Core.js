a8b3ef028c5a45e8be6816f4a9f95ed584253af1
iHad169 <27337315+iHad169@users.noreply.github.com>
2020-05-29 00:52:59 +0800

    test

--- out/production/HKNBP_Core/HKNBP_Core.js
+++ out/production/HKNBP_Core/HKNBP_Core.js
@@ -3322,7 +3322,7 @@ if (typeof kotlin === 'undefined') {
   }
   var rootURL;
   function coreVersion$lambda() {
-    return 'v2020.05_3-test11';
+    return 'v2020.05_3-test12';
   }
   var coreVersion;
   var appVersion;
--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@
  * 控制更新cache版本
  * 修改版本名今用戶進行更新新版
  */
-var coreVersion = "v2020.05_3-test11";
+var coreVersion = "v2020.05_3-test12";
 
 /**
  * 檢查依家運行係米ServiceWorker
